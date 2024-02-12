35a561e229927e1512752200bb0a077af63d6df4
BWbwchen <tim.chenbw@gmail.com>
2024-02-12 14:09:56 +0800

    [feat   ]: set max cache ttl for gpg
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- hosts/lxc/programs.nix
+++ hosts/lxc/programs.nix
@@ -5,7 +5,10 @@
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    settings = { default-cache-ttl = 86400; };
+    settings = {
+      max-cache-ttl = 86400; # valid for 1 day.
+      default-cache-ttl = 86400; # valid for 1 day.
+    };
   };
   programs.direnv.enable = true;
 }
